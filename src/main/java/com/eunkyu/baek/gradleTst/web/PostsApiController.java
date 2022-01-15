65f1e9cf56c3eea286de51c9fc8b67eeea69cb58
EUN-KYU-BAEK <dmsrb1595@naver.com>
2022-01-15 17:41:12 +0900

    - update function added - BaseTimeEntity added

--- src/main/java/com/eunkyu/baek/gradleTst/web/PostsApiController.java
+++ src/main/java/com/eunkyu/baek/gradleTst/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.eunkyu.baek.gradleTst.web;
 
-import com.eunkyu.baek.gradleTst.domain.posts.PostsService;
+import com.eunkyu.baek.gradleTst.service.posts.PostsService;
+import com.eunkyu.baek.gradleTst.web.dto.PostsResponseDto;
 import com.eunkyu.baek.gradleTst.web.dto.PostsSaveRequestDto;
+import com.eunkyu.baek.gradleTst.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,5 +15,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
