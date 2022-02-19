baec4b120413ed0faf82205661145807185478a1
EUN-KYU-BAEK <dmsrb1595@naver.com>
2022-02-19 18:19:33 +0900

    H2 DB 테스트

---
+++ src/main/java/com/eunkyu/note/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.eunkyu.note.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Getter             //(lombok anno) getter 메소드를 자동으로 추가
+@NoArgsConstructor  //(lombok anno)기본 생성자를 자동으로 추가
+@Entity             //(jpa anno)실제 DB의 테이블과 매칭될 클래스임을 명시
+public class Posts {
+    @Id //해당 테이블의 PK필드를 나타냄
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK의 생성 규칙 auto_increment
+    private Long id;
+
+    @Column(length = 500, nullable = false)     //테이블의 컬럼을 나타냄 굳이 선언하지 않아도 컬럼이 됨.
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder        //생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/eunkyu/note/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,12 @@
+/*
+    MyBatis 등에서 Dao로 불리는 DB Layer 접근자
+    Jpa에선 Repository로 부르며 인터페이스로 생성
+    생성 후 JpaRepository<Entity클래스, PK타입>을 상속
+ */
+package com.eunkyu.note.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
