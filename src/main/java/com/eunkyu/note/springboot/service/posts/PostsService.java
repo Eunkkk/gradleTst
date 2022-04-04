a1743a3fe6086d220fd40937d14097814ac1622f
EUN-KYU-BAEK <dmsrb1595@naver.com>
2022-04-04 19:33:16 +0900

    index 초기 조회 기능 추가

--- src/main/java/com/eunkyu/note/springboot/service/posts/PostsService.java
+++ src/main/java/com/eunkyu/note/springboot/service/posts/PostsService.java
@@ -8,13 +8,16 @@ package com.eunkyu.note.springboot.service.posts;
 
 import com.eunkyu.note.springboot.domain.posts.Posts;
 import com.eunkyu.note.springboot.domain.posts.PostsRepository;
+import com.eunkyu.note.springboot.web.dto.PostsListResponseDto;
 import com.eunkyu.note.springboot.web.dto.PostsResponseDto;
 import com.eunkyu.note.springboot.web.dto.PostsSaveRequestDto;
 import com.eunkyu.note.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor        //final이나 @NonNull인 필드 값만 파라미터로 받는 생성자 추가
 @Service                        //비즈니스 로직을 수행하는 클래스라는 것을 나타냄
@@ -36,10 +39,18 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
